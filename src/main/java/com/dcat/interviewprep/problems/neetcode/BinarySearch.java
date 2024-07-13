5b10e056a816cb859f00d03b5e7086bb38bf1268
Devin Catuns <devincatuns1@gmail.com>
2024-07-13 13:32:59 -0400

    binary search pass

---
+++ src/main/java/com/dcat/interviewprep/problems/neetcode/BinarySearch.java
@@ -0,0 +1,45 @@
+package com.dcat.interviewprep.problems.neetcode;
+
+public class BinarySearch {
+
+    public static int search(int[] nums, int target) {
+
+        int left  = 0;
+        int right = nums.length - 1;
+
+        while ((right - left) > 1) {
+            int index = (right + left) / 2;
+            int value = nums[index];
+
+            if (value == target) {
+                return index;
+            }
+
+            if (value < target) {
+                left = index;
+            }
+
+            if (value > target) {
+                right = index;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int case1 = search(new int[]{-1, 0, 2, 4, 6, 8}, 4);
+        System.out.println("case1 = " + case1);
+        assert case1 == 3;
+
+        int case2 = search(new int[]{-1,0,2,4,6,8}, 3);
+        System.out.println("case2 = " + case2);
+        assert case2 == -1;
+
+        int case3 = search(new int[]{5}, 5);
+        System.out.println("case3 = " + case3);
+        assert case3 == 0;
+
+
+    }
+}
